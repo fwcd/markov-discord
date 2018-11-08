5f92afc8beb665e5f38cebeb11b4b3349e5ddb32
fwcd <fwcdmail@gmail.com>
2018-11-08 23:10:51 +0100

    Working configuration

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,6 +1,36 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Lib
+-- Source: https://github.com/aquarial/discord-haskell/blob/master/examples/ping-pong.hs
 
+import Control.Exception (finally)
+import Control.Monad (when)
+import Data.Char (toLower)
+import Data.Monoid ((<>))
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+import Discord
+
+-- | Replies "pong" to every message that starts with "ping"
 main :: IO ()
-main = someFunc
+main = do
+  tok <- T.strip <$> TIO.readFile "./authtoken.secret"
+
+  dis <- loginRestGateway (Auth tok)
+
+  finally (let loop = do
+                  e <- nextEvent dis
+                  case e of
+                      Left er -> putStrLn ("Event error: " <> show er)
+                      Right (MessageCreate m) -> do
+                        when (isPing (messageText m)) $ do
+                          resp <- restCall dis (CreateMessage (messageChannel m) "Pong!" Nothing)
+                          putStrLn (show resp)
+                          putStrLn ""
+                        loop
+                      _ -> do loop
+           in loop)
+          (stopDiscord dis)
+
+isPing :: T.Text -> Bool
+isPing = T.isPrefixOf "ping" . T.map toLower
